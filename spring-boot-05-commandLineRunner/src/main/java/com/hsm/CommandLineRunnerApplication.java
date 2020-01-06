917e3263296bb8ce2574e20118af1f83b166f264
黄森明 <1586558083@qq.com>
2020-01-06 18:27:49 +0800

    commandLine

--- spring-boot-05-commandLineRunner/src/main/java/com/hsm/CommandLineRunnerApplication.java
+++ spring-boot-05-commandLineRunner/src/main/java/com/hsm/CommandLineRunnerApplication.java
@@ -1,6 +1,5 @@
-package com.neo;
+package com.hsm;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- spring-boot-05-commandLineRunner/src/main/java/com/hsm/runner/OrderRunner2.java
+++ spring-boot-05-commandLineRunner/src/main/java/com/hsm/runner/OrderRunner2.java
@@ -1,4 +1,4 @@
-package com.neo.runner;
+package com.hsm.runner;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
